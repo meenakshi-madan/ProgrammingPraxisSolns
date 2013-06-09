f659c1ae3832142cb54a2b033d8e92e848a8f7d7
meenakshi-madan <profondeur@rocketmail.com>
2013-06-10 01:08:49 +0530

    Update happy-new-year.py
    Avoid allowing expressions to pass through by floating point approximation - by importing division from future. This tip I picked up after going through the solutions other people posted.

--- happy-new-year.py
+++ happy-new-year.py
@@ -1,3 +1,5 @@
+from __future__ import division
+import itertools
 """
 Problem statement:
 As we begin the new year, we note that 109-8*7+654*3-2/1 = 2013. There are three other combinations of the numbers 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, in order, combined with the five operators NIL, +, -, * and / that also evaluate to 2013.
@@ -6,7 +8,7 @@ Your task is to write a program that finds all four expressions that evaluate to
 
 """
 
-import itertools
+
 operands = [str(i) for i in reversed(range(1, 11))]
 operators = ['', '+', '-', '/', '*']
 
